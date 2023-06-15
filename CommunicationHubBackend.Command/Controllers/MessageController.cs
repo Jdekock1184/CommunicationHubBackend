6156114c1597059087204ef960448bed7d0d0622
JDK11 <jdekock1184@gmail.com>
2023-06-15 10:54:02 +0200

    Added command files

---
+++ CommunicationHubBackend.Command/Controllers/MessageController.cs
@@ -0,0 +1,46 @@
+﻿using CommunicationHubBackend.Core.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunicationHubBackend.Command.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public MessageController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        /// <summary>
+        /// Used to analyse a message and remove any disapproved content.
+        /// </summary>
+        /// <returns>Will return a 202 Accepted if the message was analysed and approved</returns>
+        [HttpPost("sendmessage")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> SendMessage([FromBody] AnalyseMessageCommand analyseMessageCommand)
+        {
+            if (analyseMessageCommand != null)
+            {
+                try
+                {
+                    var result = await mediator.Send(analyseMessageCommand);
+                    return Ok(result);
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            return BadRequest("Message is empty");
+        }
+    }
+}
---
+++ CommunicationHubBackend.Command/Startup.cs
@@ -0,0 +1,77 @@
+﻿using CommunicationHubBackend.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace CommunicationHubBackend.Command
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = (IConfigurationRoot)configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            
+            // API
+            services.AddRouting();
+
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                                                                .AllowAnyMethod()
+                                                                .AllowAnyHeader()));
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddMediatR((x) => x.RegisterServicesFromAssemblyContaining<AssemblyMarker>());
+
+            services.AddSwaggerDocument();
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseCors("AllowAll");
+
+            //// Enable middleware to serve generated Swagger as a JSON endpoint.
+            //app.UseSwagger(c => c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+            //{
+            //    swaggerDoc.Servers = new List<OpenApiServer> {
+            //        new OpenApiServer { Url = $"", Description = "Development Server" }
+            //    };
+            //}));
+
+            //// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            //// specifying the Swagger JSON endpoint.
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint($"/swagger/v1/swagger.json", "Communication Hub Command Local");
+            //    c.SwaggerEndpoint($"", "Communication Hub Command Development");
+            //    c.RoutePrefix = string.Empty;
+            //});
+
+            app.UseRouting();
+
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
