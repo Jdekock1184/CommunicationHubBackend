c331514094f1a39e62a1ace1c126534f75575b4c
JDK11 <jdekock1184@gmail.com>
2023-06-16 21:29:35 +0200

    Db connected

---
+++ CommunicationHubBackend.Core/Context/SensitiveWordsContext.cs
@@ -0,0 +1,43 @@
+﻿using CommunicationHubBackend.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationHubBackend.Core.Context
+{
+    public partial class SensitiveWordsContext : DbContext
+    {
+        public SensitiveWordsContext() 
+        { 
+        }
+
+        public virtual DbSet<TblSensitiveWords> TblSensitiveWords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:comshub.database.windows.net,1433;Initial Catalog=SensitivesWords;Persist Security Info=False;User ID=ComsAdmin;Password=Coms@Flash;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblSensitiveWords>(entity =>
+            {
+                entity.ToTable("tbl_sensitive_words").HasNoKey();
+
+                entity.Property(e => e.Word)
+                    .HasColumnName("word")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- CommunicationHubBackend.Core/Handlers/AnalyseMessageHandler.cs
+++ CommunicationHubBackend.Core/Handlers/AnalyseMessageHandler.cs
@@ -1,6 +1,8 @@
 ﻿using CommunicationHubBackend.Core.Commands;
+using CommunicationHubBackend.Core.Context;
 using CommunicationHubBackend.Core.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,12 +17,14 @@ namespace CommunicationHubBackend.Core.Handlers
         public async Task<string> Handle(AnalyseMessageCommand request, CancellationToken cancellationToken)
         {
             string[] allWordsInmessage = request.Message.Split(' ');
-            foreach (var item in SensitiveWords.SensitiveWordsList)
+            var sensitiveWordsContext = new SensitiveWordsContext();
+
+            foreach (var item in sensitiveWordsContext.TblSensitiveWords.AsNoTracking())
             {
                 for (int i = 0; i < allWordsInmessage.Length; i++)
                 {
                     string cleanedWord = Regex.Replace(allWordsInmessage[i], @"[\p{P}]", "");
-                    if (cleanedWord.Equals(item))
+                    if (cleanedWord.Equals(item.Word))
                     {
                         string punctuationMarks = Regex.Replace(allWordsInmessage[i], @"[\p{L}]+", "");
                         allWordsInmessage[i] = "****";
